b45fb92296cb3b6bf08324657f086cab762f0aeb
LoganCabanaw <lcabanaw@uwyo.edu>
2024-10-20 20:46:32 -0600

    Update cosc1010-lab06.py

--- cosc1010-lab06.py
+++ cosc1010-lab06.py
@@ -1,12 +1,11 @@
-# Your Name Here
+# Logan Cabanaw
 # UWYO COSC 1010
-# Submission Date
+# 10/20/2024
 # Lab 06
-# Lab Section: 
+# Lab Section: 16
 # Sources, people worked with, help given to: 
-# your
-# comments
-# here
+# https://www.w3schools.com/python/ref_func_chr.asp : The chr() I used
+# https://www.w3schools.com/python/ref_func_ord.asp : The ord() I used
 
 
 random_string = """
@@ -61,8 +60,10 @@ lrslowamkcwolbcgfkfciegdwqskuazxnycqkkggzsowcmafay
 ibmkdwkqmdkjesqnjiqpijixbwjhenmsrrlpcseliiajlvcaac
 zkdenxczyooloczcaahnkehbwimvieedpdlqfafbqvxvfmvabd
 """
-random_string = random_string.replace("\n","") #remove all newline characters
-print(len(random_string)) # Print out the size for reference 
+
+
+
+
 
 # Above is a string with 2500 characters.
 # Create a program that goes through and counts the occurrence of each character, excluding \n using a  dictionary
@@ -84,17 +85,41 @@ print(len(random_string)) # Print out the size for reference
 #Will need to first declare a dictionary 
 
 # Output: each letter and its corresponding occurrence in alphabetical order
+random_string = random_string.replace("\n","") #remove all newline characters
+print(len(random_string))# Print out the size for reference 
+
+Alphabet = [chr(letter) for letter in range(ord("a"), ord("z")+1)]
+Occurence_Dict = {}
+Percentage_Dict = {}
+
+for letter in Alphabet: 
+    num = 0
+    for char in random_string:
+        if letter == char:
+            num = num + 1
+    Occurence_Dict[letter] = num
+print(Occurence_Dict)
 
 print("*"*75)
-# Output which letter occurred the most 
 
-most_occurred = ""
-least_occurred = ""
+# Output which letter occurred the most 
+bucket_big = Occurence_Dict["a"]
+bucket_small = Occurence_Dict["a"]
+for letter in Occurence_Dict:
+    Percentage_Dict[letter] = f"{((Occurence_Dict[letter])/len(random_string))*100}%"
+    if Occurence_Dict[letter] < bucket_small:
+        bucket_small = Occurence_Dict[letter]
+    if Occurence_Dict[letter] > bucket_big:
+        bucket_big = Occurence_Dict[letter]
+
+most_occurred = bucket_big
+least_occurred = bucket_small
 
 print(f"The letter that occurred the most is {most_occurred}")
 print("*"*75)
 # Output which letter occurred the least 
-print(f"The letter that occurred the most is {least_occurred}")
+print(f"The letter that occurred the least is {least_occurred}")
 print("*"*75)
 
 # Output what the percentage of the string each character is, again in alphabetical
+print(Percentage_Dict)
